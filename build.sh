efc05e9e2bf71f558c590973349ccfb90c3c2179
Danijel Sipos <danisipos1@gmail.com>
2025-10-15 10:40:36 +0200

    Refactor build configuration and image handling; update CMakeLists.txt to dynamically include images and remove obsolete files

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
-# rm -r build
-# mkdir build
+rm -r build
+mkdir build
 cd build
-cmake -DARM_GCC_PATH=/home/dani/projects/tq/lcd-gui-tester/build/libraries/arm-gnu-toolchain -DNRF_SDK_PATH=/home/dani/projects/tq/lcd-gui-tester/build/libraries/nrf5_sdk -DLVGL_PATH=/home/dani/projects/tq/lcd-gui-tester/build/libraries/lvgl -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DARM_GCC_PATH=/home/dani/projects/tq/lcd-gui-tester/build/libraries/arm-gnu-toolchain \
+      -DNRF_SDK_PATH=/home/dani/projects/tq/lcd-gui-tester/build/libraries/nrf5_sdk \
+      -DLVGL_PATH=/home/dani/projects/tq/lcd-gui-tester/build/libraries/lvgl \
+      -DIMAGES_PATH=/home/dani/projects/tq/lcd-gui-tester/build/generated \
+      -DCMAKE_BUILD_TYPE=Debug ..
 make -j8
